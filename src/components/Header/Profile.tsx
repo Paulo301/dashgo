be2b151585f3375ef3b0565d77919a18d7435786
Paulo301 <paulovictor1918@gmail.com>
2023-06-02 16:34:52 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,16 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-interface ProfileProps {}
+interface ProfileProps {
+  showProfileData?: boolean;
+}
 
-export function Profile({}: ProfileProps) {
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align='center'>
-      <Box mr='4' textAlign='right'>
-        <Text>Paulo Victor Lemos</Text>
-        <Text color='gray.300' fontSize='small'>
-          paulolemos.dev@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Paulo Victor Lemos</Text>
+          <Text color='gray.300' fontSize='small'>
+            paulolemos.dev@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size='md'
