2485e83434797bbbf1f62ee26f647c7e78b03e08
Paulo301 <paulovictor1918@gmail.com>
2023-06-17 12:59:01 -0300

    Lógica de paginação

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,5 +1,6 @@
-import { createServer, Factory, Model } from 'miragejs';
+import { createServer, Factory, Model, Response } from 'miragejs';
 import { faker } from '@faker-js/faker';
+import { RouteHandler } from 'miragejs/server';
 
 type User = {
   name: string;
@@ -35,7 +36,23 @@ export function makeServer() {
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (this: any, schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user'))
+          .users.slice(pageStart, pageEnd);
+
+        return new Response(
+          200,
+          { 'x-total-count': String(total) },
+          { users }
+        );
+      });
       this.post('/users');
 
       this.namespace = '';
